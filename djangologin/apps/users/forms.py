a58ad5e6a93da2b0fe5d0413804d97fa3ec07bff
Roberth Solís <roberth.solis@gmail.com>
2016-05-10 13:41:11 -0600

    Form commet added

--- djangologin/apps/users/forms.py
+++ djangologin/apps/users/forms.py
@@ -9,3 +9,15 @@ class LoginForm(forms.ModelForm):
         fields = (
             'username', 'password'
         )
+
+
+# Esta es otra alternativa que puedes usar
+"""
+class LoginForm(forms.Form):
+
+    username = forms,CharField(max_length=50)
+    username = forms,CharField(max_length=50,
+     widget = forms.TextInput( attrs = {
+        'type' : 'password'
+     }))
+"""
