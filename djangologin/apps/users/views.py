b3ffa72c55012a1b0da74d638b1ff3539e5d33d0
Roberth Solís <roberth.solis@gmail.com>
2016-05-14 13:54:55 -0600

    view of login in progress

--- djangologin/apps/users/views.py
+++ djangologin/apps/users/views.py
@@ -1,41 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, authenticate, login
+from django.views.generic import FormView
 
 from .foms import LoginForm
 
 
-def login_view(request):
+class LoginView(FormView):
 
-    if request.user.is_athenticated():
+    form_class = LoginForm
 
-        redirect('/')
-
-    elif request.POST:
-
-        login_form = LoginForm(request.POST)
-
-        if login_form.is_valid:
-
-            user = authenticate(username=login_form.cleaned_data['username'],
-                                password=login_form.cleaned_data['password'])
-
-            if user is not None:
-                login(user)
-                return redirect('/')
-            else:
-                login_form = LoginForm()
-
-        else:
-            login_form = LoginForm()
-
-            form_data = {
-              'form': login_form
-            }
-
-    else:
-        login_form = LoginForm()
-
-    return render(request, 'login.html', form_data)
+    return render(request, 'login.html')
 
 
 def Logout(request):
