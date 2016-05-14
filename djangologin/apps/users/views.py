42454ee88f6b1eca10ec39b3996518d1fc37182b
Roberth Solís <roberth.solis@gmail.com>
2016-05-14 14:23:38 -0600

    view login finished

--- djangologin/apps/users/views.py
+++ djangologin/apps/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 from django.views.generic import FormView
 
 from .forms import LoginForm
@@ -9,9 +9,16 @@ class LoginView(FormView):
 
     form_class = LoginForm
     template_name = 'login.html'
-    success_url = '/'
+    success_url = '/login/'
 
     def form_valid(self, form):
+
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+
+        print(user)
+
+        if user is not None and user.is_active:
+            login(self.request, user)
         return super(LoginView, self).form_valid(form)
 
 
